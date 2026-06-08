2a5f631e6e401859273d4849405e9b8304d8a0fb
Sebastian Jorge Medrano Chacolla <sebastian11medrano@gmail.com>
2026-06-08 00:55:03 +0000

    feat: actualizar perfil y materias

--- resources/views/materias.blade.php
+++ resources/views/materias.blade.php
@@ -18,36 +18,34 @@
 
 <div class="caja">
     <h2>Notas</h2>
-    <div class="contenedor-tabla">
-        <table>
-            <thead>
-                <tr>
-                    <th>Código</th>
-                    <th>Materia</th>
-                    <th>Créditos</th>
-                    <th>Nota</th>
-                    <th>Estado</th>
-                </tr>
-            </thead>
-            <tbody>
-                {{--
-                 Diferencia clave respecto al Día 12:
-                 ANTES: $materia['codigo'] → acceso a índice de array
-                 AHORA: $m->getCodigo() → llamada a método de objeto
-                 La vista no sabe ni le importa cómo se construyó el objeto.
-                 Eso es encapsulamiento aplicado al MVC.
-                --}}
-                @foreach ($materias as $m)
-                <tr style="background: {{ $m->getColorEstado() }}">
-                    <td>{{ $m->getCodigo() }}</td>
-                    <td>{{ $m->getNombre() }}</td>
-                    <td>{{ $m->getCreditos() }}</td>
-                    <td><strong>{{ $m->getNota() }}</strong></td>
-                    <td><strong>{{ $m->getEstado() }}</strong></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Materia</th>
+                <th>Créditos</th>
+                <th>Nota</th>
+                 <th>Estado</th>
+             </tr>
+        </thead>
+        <tbody>
+            {{--
+              Diferencia clave respecto al Día 12:
+              ANTES: $materia['codigo'] → acceso a índice de array
+              AHORA: $m->getCodigo() → llamada a método de objeto
+              La vista no sabe ni le importa cómo se construyó el objeto.
+              Eso es encapsulamiento aplicado al MVC.
+            --}}
+            @foreach ($materias as $m)
+            <tr style="background: {{ $m->getColorEstado() }}">
+                <td>{{ $m->getCodigo() }}</td>
+                <td>{{ $m->getNombre() }}</td>
+                <td>{{ $m->getCreditos() }}</td>
+                <td><strong>{{ $m->getNota() }}</strong></td>
+               <td><strong>{{ $m->getEstado() }}</strong></td>
+             </tr>
+         @endforeach
+         </tbody>
+    </table>
 </div>
 @endsection
